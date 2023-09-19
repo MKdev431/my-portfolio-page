2043ae1b6334dd443b3bcb3b9f849d3ca5741c57
Michal Kurek (Mike) <michal@kurek.pl>
2023-09-19 15:39:44 +0200

    project start

---
+++ src/layouts/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "../styles/App.scss";
+
+class App extends Component {
+  state = {};
+  render() {
+    return <h1>działa</h1>;
+  }
+}
+
+export default App;
